2bbe0aa983c44032195ad44cb1250fbf249c1c8c
guitarrockstarco <guitarrockstarco@yahoo.com>
2020-01-14 19:28:21 -0500

    Updates

---
+++ precache-manifest.5ddf22a8f862506d94376d6179b7e4aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b17fdde0cccf3e01dde0c3ec2100b68",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2275d726a01c88b46962",
+    "url": "/monsters-rolodex/static/css/main.36112bcc.chunk.css"
+  },
+  {
+    "revision": "1093a6240466003a5984",
+    "url": "/monsters-rolodex/static/js/2.3a82cee6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.3a82cee6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2275d726a01c88b46962",
+    "url": "/monsters-rolodex/static/js/main.8ceb600f.chunk.js"
+  },
+  {
+    "revision": "fcc6c7087f08bfeac99a",
+    "url": "/monsters-rolodex/static/js/runtime-main.52149680.js"
+  }
+]);
